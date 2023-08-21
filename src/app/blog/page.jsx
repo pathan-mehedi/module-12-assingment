ca60e4665ea28b061e9bb5d68b2629b37bae342a
Mehedi Pathan <eity.mehedipathan@gmail.com>
2023-08-21 11:17:32 +0600

    blog and single blog are ready to deploy v_003

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Link from "next/link";
 
 const Blog = () => {
-  return (
-    <div>Blog Page</div>
-  )
-}
+  const [categories, setCategories] = useState([]);
+  const [blogItems, setBlogItems] = useState([]);
 
-export default Blog
+  useEffect(() => {
+    // Fetch categories
+    axios
+      .get("https://basic-blog.teamrabbil.com/api/post-categories")
+      .then((response) => {
+        setCategories(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching categories:", error);
+        setCategories([]);
+      });
+
+    // Fetch blog items
+    axios
+      .get("https://basic-blog.teamrabbil.com/api/post-list/2")
+      .then((response) => {
+        setBlogItems(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching blog items:", error);
+        setBlogItems([]);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <section className="categoriesSection">
+        <h2 className="categoriesTitle">Categories</h2>
+        <ul className="categoriesList">
+          {categories.map((category) => (
+            <li key={category.id} className="categoryItem">
+              <Link href={`/blog?category=${category.slug}`}>
+                <Link className="categoryLink">{category.name}</Link>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className="blogSection">
+        <h2 className="blogTitle">Latest Blog Posts</h2>
+        <div className="blogItems">
+          {blogItems.map((item) => (
+            <div key={item.id} className="blogItem">
+              <h3 className="blogItemTitle">{item.title}</h3>
+              <p className="blogItemDate">Created: {item.created_at}</p>
+              <p className="blogItemDate">Updated: {item.updated_at}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Blog;
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
 
-const contact = () => {
+const Contact = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    setIsSubmitted(true);
+  };
+
   return (
-    <div>contact</div>
-  )
-}
+    <div className='container'>
+      <div className='formSection'>
+        <h2 className='formTitle'>Contact Us</h2>
+        {isSubmitted ? (
+          <p className='successMessage'>
+            Thank you for contacting MP Blog! We will get back to you soon.
+          </p>
+        ) : (
+          <form className='contactForm' onSubmit={handleSubmit}>
+            <label className='formLabel'>Name</label>
+            <input type='text' className='formInput' />
 
-export default contact
+            <label className='formLabel'>Email</label>
+            <input type='email' className='formInput' />
+
+            <label className='formLabel'>Website</label>
+            <input type='url' className='formInput' />
+
+            <label className='formLabel' >Message</label>
+            <textarea className='formTextarea formTextArea'></textarea>
+
+            <button type='submit' className='submitButton'>
+              Submit
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
